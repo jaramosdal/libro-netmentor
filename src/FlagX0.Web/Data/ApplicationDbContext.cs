ac859027a4cf9d93b7fb4aef02fe849bbea6ef24
Javier Ramos Nodal <javiramosnodal@gmail.com>
2024-04-28 10:51:44 +0200

    56

--- src/FlagX0.Web/Data/ApplicationDbContext.cs
+++ src/FlagX0.Web/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FlagX0.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FlagX0.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<FlagEntity> Flags { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ src/FlagX0.Web/Data/Entities/FlagEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlagX0.Web.Data.Entities;
+
+public class FlagEntity
+{
+    [Key] public int Id { get; set; }
+    public required string Name { get; set; }
+    public IdentityUser User { get; set; }
+    public required virtual string UserId { get; set; }
+    public required bool Value { get; set; }
+}
