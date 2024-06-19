c4ab24f69f15d26990190d92f0d8a17dd779576b
Javier Ramos Nodal <javiramosnodal@gmail.com>
2024-06-19 20:43:09 +0200

    Unit Tests.

---
+++ tests/FlagX0.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FlagX0.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/FlagX0.UnitTests/Web/Business/UserInfo/FlagUserDetailsStub.cs
@@ -0,0 +1,13 @@
+﻿using FlagX0.Web.Business.UserInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagX0.UnitTests.Web.Business.UserInfo;
+
+internal class FlagUserDetailsStub : IFlagUserDetails
+{
+    public string UserId => "1";
+}
